900c3d1c92827e58102bb61005b297b79feccab6
dhleebb42 <55428726+dhleebb42@users.noreply.github.com>
2019-10-10 15:53:06 +0900

    06_1010(2)

--- main.c
+++ main.c
@@ -3,20 +3,22 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-void print_star()
+int sumTwo( int a, int b)
 {
-	int i;
-	for(i=0; i<10; i++)
-		printf("*");
+	int output;
+	output = a+b;
+	return output;
 }
 
-int main(int argc, char *argv[]) {
-	int i;
+int square(int n)
+{
+	return n*n;
+}
 	
-	print_star();
-	print_star();
-	print_star();
+int get_max(int a, int b)
+{
+	if(a>b) return (a);
+	else return (b);
 	
 	return 0;
 }
